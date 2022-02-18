e00a36f0cd270159eea41fe9d38999f41ed52a98
RITIK <64574373+RitikSingh20@users.noreply.github.com>
2022-02-18 10:47:36 +0530

    Add files via upload

---
+++ src/containers/App/Router.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable */
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Layout from '../Layout/index';
+import MainWrapper from './MainWrapper';
+
+import LogIn from '../LogIn/index';
+import ExamplePageOne from '../Example/index';
+import ExamplePageTwo from '../ExampleTwo/index';
+import Usermanagement from '../UserManagement/components/Usermanagement'
+
+const Pages = () => (
+  <Switch>
+    <Route path="/pages/one" component={ExamplePageOne} />
+    <Route path="/pages/two" component={ExamplePageTwo} />
+  </Switch>
+);
+
+const Pages2 = () => (
+  <Switch>
+    <Route path="/um" component={Usermanagement} />
+ 
+  </Switch>
+);
+
+const wrappedRoutes = () => (
+  <div>
+    <Layout />
+    <div className="container__wrap">
+      <Route path="/pages" component={Pages} />
+      <Route path="/um" component={Pages2} />
+    </div>
+  </div>
+);
+
+const Router = () => (
+  <MainWrapper>
+    <main>
+      <Switch>
+        <Route exact path="/" component={LogIn} />
+        <Route exact path="/log_in" component={LogIn} />
+        <Route path="/" component={wrappedRoutes} />
+      </Switch>
+    </main>
+  </MainWrapper>
+);
+
+export default Router;
